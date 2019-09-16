6913c2c2e37a8631628cbd7e2a5925672237b04d
Stefan Holmberg <stefan.holmberg@systementor.se>
2019-09-16 13:06:41 +0200

    Add project files.

---
+++ ListDemosCms/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListDemosCms
+{
+    class Program
+    {
+        static double CalculateAverage(List<int> allaTal)
+        {
+            int summaAllaTal = 0;
+            foreach (var i in allaTal)
+                summaAllaTal += i;
+            return Convert.ToDouble(summaAllaTal) /
+                Convert.ToDouble(allaTal.Count);
+        }
+        static void Main(string[] args)
+        {
+            var listaMedTal = new List<int>();
+            while(true)
+            {
+                Console.WriteLine("Mata in ett tal");
+                var tal = Convert.ToInt32(Console.ReadLine());
+                if (tal == 0) break;
+
+                listaMedTal.Add(tal);
+
+                var medel = CalculateAverage(listaMedTal);
+                Console.WriteLine($"Medel på alla är {medel}");
+
+            }
+        }
+    }
+}
